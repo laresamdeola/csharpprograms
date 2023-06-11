cfd60624ee7bea928d9e8b7a4a053a1243f22c26
laresamdeola <74225315+laresamdeola@users.noreply.github.com>
2023-06-11 12:51:15 +0000

    changed the control flow syntax from an if statement to a switch statement

--- parcel.cs
+++ parcel.cs
@@ -10,11 +10,25 @@ class Parcels {
     place = Console.ReadLine();
     //Console.WriteLine(place.Substring(0,2));
     
-    if(place.Substring(0,2).Equals("BL")){
-      postCodeAddress = "Bolton";
-      Console.WriteLine(postCodeAddress);
-    } else {
-      Console.WriteLine("Place does not exist");
+    if(place.Substring(0).Equals("M")) {
+      postCodeAddress = "Manchester";
     }
+    
+    switch(place.Substring(0,2)) {
+    case "BL":
+      postCodeAddress = "Bolton";
+      break;
+    case "SK":
+      postCodeAddress = "Stockport";
+      break;
+    case "OL":
+      postCodeAddress = "Oldham";
+      break;
+    default:
+      postCodeAddress = "This is not a valid postcode";
+      break;
+    }
+
+    Console.WriteLine(postCodeAddress);
   }
 };
